63a3b55a460442bb7768aff57344d9a989f0834b
balaji-sridhar <balaji.subramanian@ucdconnect.ie>
2018-06-10 18:24:17 +0100

    Clean up of the workspace

--- Calculate_Area.py
+++ Calculate_Area.py
@@ -1,5 +1,10 @@
 import numpy as np
 from sklearn import metrics
+
+# To calculate the area under the curve. The area difference two curve is measured.
+# One of the curve is plotted using the data from the Aggregated System Demand
+# Vs Individual Smart Meter Data
+
 y = np.array([1, 1, 2, 2])
 #pred = np.array([0.1, 0.4, 0.35, 0.8])
 pred = np.array([1, 1, 2, 2])
